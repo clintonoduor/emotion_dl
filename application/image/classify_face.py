489ed7f3b6d6b9e6926a3d1fee5a5c3e0c8d08f8
clintonoduor <clintonoduor3@gmail.com>
2022-07-28 15:19:07 +0500

    added files

--- application/image/classify_face.py
+++ application/image/classify_face.py
@@ -96,6 +96,18 @@ def main(argv):
 
             next_frame = 0 # limit to ~10 fps here
 
+            # Load the cascade
+            face_cascade = cv2.CascadeClassifier('face_detect.xml')
+            # Read the input image
+            img = cv2.imread('test.jpg')
+            # Convert into grayscale
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Detect faces
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            # Draw rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
             for res, img in runner.classifier(videoCaptureDeviceId):
                 if (next_frame > now()):
                     time.sleep((next_frame - now()) / 1000)
---
+++ application/image/music.py
@@ -0,0 +1,4 @@
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('sample.mp3')
+pygame.mixer.music.play()
